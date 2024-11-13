f7f3da51815e4e6496d38c8bf56013edc9f0bc4c
RIBOIMAN1 <rileybenson@my.whccd.edu>
2024-11-13 03:12:36 -0800

    index.html: - Added my favorite link, the Terraria Wiki, which opens in a new window.
    script.js:
    - Added a set of commands that contain event listeners for a button and a link, giving the user an alert that can last multiple clicks, also blocking the user from accessing the website.
    - Added a comment describing the code that involves the favorite website.

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ window.onload = function() {
     };
     button.addEventListener("click", buttonClickHandler);
 } // Contains an event listener, displaying an alert for only one click
+window.onload = function() {
+    var button = document.getElementById("ExampleButton");
+    var buttonClickHandler = function() {
+        alert("Hello World!");
+        button.removeEventListener("click", buttonClickHandler);
+    };
+    button.addEventListener("click", buttonClickHandler);
+
+    var link = document.getElementById("exampleLink");
+    link.addEventListener("click", function(event) {
+        event.preventDefault();
+        alert("No distractions! I’m coding!");
+    });
+} // Contains event listeners that make sure there is an alert whenever you click the link
